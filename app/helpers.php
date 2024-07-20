e8e9acc4608d505b0570a028581ac4d5708d8cc8
PaulAndrianoff <pandrianoff@gmail.com>
2024-07-21 00:16:55 +0200

    add helper functions

--- app/helpers.php
+++ app/helpers.php
@@ -1,7 +1,22 @@
 <?php
 
+require_once __DIR__ . '/../config.php';
+
 if (!function_exists('dd')) {
     function dd(...$vars) {
         Debugger::dd(...$vars);
+    }
+}
+
+if (!function_exists('getConfig')) {
+    function getConfig($value) {
+        $config = require __DIR__ . '/../config.php';
+        return $config[$value];
+    }
+}
+
+if (!function_exists('redirect')) {
+    function redirect($route) {
+        header('Location: ' . getConfig('url') . $route);
     }
 }
